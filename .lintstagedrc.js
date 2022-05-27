03010eaef24fd15924cb825038ae8d7ff1933fc1
Jiaxiang Xi <jiaxiang.xi@eisgroup.com>
2022-05-27 16:25:16 +0800

    ddd

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "*.{js,jsx,ts,tsx}": "eslint packages/**/src/*.{js.jsx,ts,tsx,json} --fix"
+    "packages/**/src/*.{js,jsx,ts,tsx}": "eslint packages/**/src/*.{js.jsx,ts,tsx,json} --fix"
 }
