7e3bc1888dd20aae73ab99ad8844eafc50d93ddb
Jiaxiang Xi <jiaxiang.xi@eisgroup.com>
2022-09-06 13:02:31 +0800

    init: added swc to mon1

--- packages/mono2/src/App.tsx
+++ packages/mono2/src/App.tsx
@@ -1,5 +1,10 @@
+import { CustomInput } from '@demo/mono1'
 import React, { FC } from 'react'
 
 export const App: FC<{}> = () => {
-  return <div>Test Me</div>
+  return (
+    <div>
+      <CustomInput id={'rest'} />
+    </div>
+  )
 }
