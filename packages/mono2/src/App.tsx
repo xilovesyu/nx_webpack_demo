b2437c7395a666a7478a34159d5f135f9d9e71fd
Jiaxiang Xi <jiaxiang.xi@eisgroup.com>
2023-02-03 10:28:43 +0800

    init: test github actions

--- packages/mono2/src/App.tsx
+++ packages/mono2/src/App.tsx
@@ -2,9 +2,5 @@ import { CustomInput } from '@demo/mono1'
 import React, { FC } from 'react'
 
 export const App: FC<{}> = () => {
-  return (
-    <div>
-      <CustomInput id={'rest'} />
-    </div>
-  )
+  return <CustomInput id={'rest'} />
 }
